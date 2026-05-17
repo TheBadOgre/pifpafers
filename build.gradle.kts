49ad32a4c7c4c6fd4de018b929acf3dc51795796
Rafkos <admin@rafkos.net>
2026-05-17 02:47:24 +0200

    chore: add serialization, coroutines, log4j, junit5, mockito deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,32 @@
 plugins {
     kotlin("jvm") version "2.3.10"
+    kotlin("plugin.serialization") version "2.3.10"
+    application
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "net.rafkos.neuroshima.editor"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.24.3")
+    implementation("org.apache.logging.log4j:log4j-core:2.24.3")
+
     testImplementation(kotlin("test"))
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("net.rafkos.neuroshima.editor.app.MainKt")
 }
 
 kotlin {
